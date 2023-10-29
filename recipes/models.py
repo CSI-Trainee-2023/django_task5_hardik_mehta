3994c43e60cb40be3dab8066372d749c8d505e0c
Hardik <hardik2213105@akgec.ac.in>
2023-10-29 16:12:58 +0530

    recipe creation form created and url added

---
+++ recipes/models.py
@@ -0,0 +1,19 @@
+from django.urls import reverse
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class Recipe(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+
+    auther = models.ForeignKey(User,on_delete=models.CASCADE)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def get_absolute_url(self):
+        return reverse("recipes-detail", kwargs = {"pk":self.pk})
+
+    def __str__(self):
+        return self.title
---
+++ recipes/views.py
@@ -0,0 +1,61 @@
+from django.forms.models import BaseModelForm
+from django.http import HttpResponse
+from django.shortcuts import render,HttpResponse
+from django.views.generic import ListView,DetailView,CreateView,UpdateView,DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin,UserPassesTestMixin
+from . import models
+from django.urls import reverse_lazy
+
+# Create your 
+def home(request):
+    recipes = models.Recipe.objects.all()
+    context = {
+        'recipes' : recipes
+    }
+    return render(request,"recipes/home.html",context)
+
+
+class RecipeListView(ListView):
+    model = models.Recipe
+    template_name = 'recipes/home.html'
+    context_object_name = 'recipes'
+
+
+class RecipeDetailView(DetailView):
+    model = models.Recipe
+
+def about(request):
+    return render(request,"recipes/about.html",{'title':'about us page'})
+   
+
+class RecipeCreateView(LoginRequiredMixin,CreateView):
+    model = models.Recipe
+    fields = ['title','description']
+
+    def form_valid(self,form):
+        form.instance.auther = self.request.user
+        return super().form_valid(form)
+    
+
+class RecipeUpdateView(LoginRequiredMixin,UserPassesTestMixin,UpdateView):
+    model = models.Recipe
+    fields = ['title','description']
+
+
+    def test_func(self):
+        recipe = self.get_object()
+        return self.request.user == recipe.auther
+
+    def form_valid(self,form):
+        form.instance.auther = self.request.user
+        return super().form_valid(form)
+    
+
+class RecipeDeleteView(LoginRequiredMixin,UserPassesTestMixin,DeleteView):
+    model = models.Recipe
+    success_url = reverse_lazy('recipes-home')
+
+
+    def test_func(self):
+        recipe = self.get_object()
+        return self.request.user == recipe.auther
